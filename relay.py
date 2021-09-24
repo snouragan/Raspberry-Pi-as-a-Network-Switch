47907b09a6e5032825d7611b5a5848083d29660a
snouragan <ahaiducescu@yahoo.com>
2021-09-24 17:08:36 +0300

    Pi knows which relay is on

--- relay.py
+++ relay.py
@@ -4,11 +4,14 @@
 # 0 - relay off
 
 class Relay:
-    
+    state = 0b11111111
+
     def __init__(self, number):
         self.number = number
     def getNumber(self):
         return self.number
+    def isOn(self):
+        return False if self.state & 1<<self.getNumber() else True 
 
 def TEST_1():
     relay = Relay(0)
@@ -20,9 +23,15 @@ def TEST_2():
         relays.append( Relay(_))
     for relay in relays:
         print(relay.getNumber(), sep=' ', end = '\n')
-
+def TEST_3():
+    relays = []
+    for _ in range(8):
+        relays.append( Relay(_))
+    Relay.state = 0b00100100
+    for relay in relays:
+        print (relay.isOn(), sep =' ', end = '\n')
 if __name__ == '__main__':
     #TEST_1()
     #TEST_2()    
-    #TEST_3()
+    TEST_3()
     
