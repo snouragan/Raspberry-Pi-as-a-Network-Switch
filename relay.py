d4de950a5c3aee8399b196f646dd82eaaa2b9f91
snouragan <ahaiducescu@yahoo.com>
2021-10-01 18:45:21 +0300

    Relays scale with quantity and terminal size

--- relay.py
+++ relay.py
@@ -8,14 +8,16 @@ from curses import wrapper, textpad
 
 class Relay:
     state = 0b11111111
-    numberOfRelays = 8 
+    numberOfRelays = 6 
     stdscr = curses.initscr()
     SCREEN_WIDTH = curses.COLS
     SCREEN_HEIGHT = curses.LINES
-    WIDTH = 8
-    HEIGHT = 8
-    SPACE = 0
-    STARTY = 1
+    WIDTH = SCREEN_WIDTH//10
+    HEIGHT = SCREEN_HEIGHT//3
+    SPACE = (SCREEN_WIDTH - (WIDTH*numberOfRelays)) // (numberOfRelays+1)
+    MARGINX = (SCREEN_WIDTH - (numberOfRelays*WIDTH + (numberOfRelays+1)*SPACE)) // 2
+    MARGINY = 1
+
     def __init__(self, number, name="Relay"):
         self.name = name
         self.number = number
@@ -26,32 +28,29 @@ class Relay:
         return self.number
     def isOn(self):
         return False if self.state & 1<<self.getNumber() else True 
-    def printParameters(self):
-        Relay.stdscr.clear()
-        Relay.stdscr.addstr(0, 0, str(Relay.SCREEN_WIDTH))
-        Relay.stdscr.addstr(1, 0, str(Relay.SCREEN_HEIGHT))
-    def setSpace():
-        Relay.SPACE = (Relay.SCREEN_WIDTH - (Relay.WIDTH*Relay.numberOfRelays)) // (Relay.numberOfRelays+1)
     def drawRelay(self):
-        Relay.setSpace()
-        uly = Relay.STARTY
-        ulx = Relay.SPACE*(self.number+1) + self.number*Relay.WIDTH 
-        lry = Relay.STARTY + Relay.HEIGHT
+        uly = Relay.MARGINY
+        ulx = Relay.MARGINX + Relay.SPACE*(self.number+1) + self.number*Relay.WIDTH 
+        lry = Relay.MARGINY + Relay.HEIGHT
         lrx = ulx + Relay.WIDTH
         curses.textpad.rectangle(Relay.stdscr, uly, ulx, lry, lrx)
         Relay.stdscr.refresh()
     def drawDetails(self):
         pass
+    def debug(self):
+        print("WIDTH:", Relay.WIDTH, "HEIGHT:", Relay.HEIGHT, "SPACE:", Relay.SPACE, "MARGINX:", Relay.MARGINX, "uly:", Relay.MARGINY, "ulx:", Relay.MARGINX + Relay.SPACE*(self.number+1) + self.number*Relay.WIDTH, "lry:",Relay.MARGINY + Relay.HEIGHT, "lrx:", Relay.MARGINX + Relay.SPACE*(self.number+1) + self.number*Relay.WIDTH + Relay.WIDTH)
+ 
+
 if __name__ == '__main__':
     try:
-        numberOfRelays = 8
+        numberOfRelays = 6
         relay = []
         for _ in range(numberOfRelays):
             relay.append(Relay(_))
-#        for _ in range(1, numberOfRelays+1):
-#            Relay.numberOfRelays = _
-#            print(Relay.numberOfRelays)
-#            Relay.setSpace()
+            relay[_].debug()
+#        print '{}'.format(Relay.stdscr.getmaxyx())
+#        for _ in range(numberOfRelays):
+#                relay[_].drawRelay()
         while True:
             for _ in range(numberOfRelays):
                 relay[_].drawRelay()
@@ -63,3 +62,10 @@ if __name__ == '__main__':
         curses.endwin()
         print('\nInterrupted')
         sys.exit(0)
+    finally:
+        curses.nocbreak()
+        Relay.stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
+
+
